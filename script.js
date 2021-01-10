fe3ca203cc57099a1a909b34dda1888fba3fde6e
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-10 11:23:47 -0600

    psuedo code is done

---
+++ script.js
@@ -0,0 +1,12 @@
+// SNAIL CODING CHALLENGE
+// Create a function that accepts an array. Given an n x n array return the arrray numbers from outermost numbers to the middle. Traveling clockwise.
+
+// PSUEDO CODE
+    // create a function that accepts an array
+    // create an empty array that will serve as the final array
+    // make a loop that goes over the entire array
+        // get the first row (the first array)
+        // get items at the end of all arrays to create right side of snail
+        // reverse the bottom row
+        // get beginning items of the array to create left side
+
