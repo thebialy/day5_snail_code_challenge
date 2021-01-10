9c7e53232c135c5160f1ea49cb182b5c09fa14d7
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-10 11:43:48 -0600

    finished

--- script.js
+++ script.js
@@ -10,17 +10,20 @@
         // reverse the bottom row
         // get beginning items of the array to create left side
 
-const snail = (arr) => {
-  const snailArray = [];
-  while(arr.length){
-      snailArray.push(...arr.shift())
-      for(let i = 0; i < arr.length; i++) {
-          snailArray.push(arr[i].pop())
-      }
-      snailArray.push(...snailArray(arr.pop() || []).reverse())
-      for (let i = arr.length -1; i>= 0; i--) {
-          snailArray.push(array[i].shift())
-      }
-  }
-  return snailArray
-}
+const snail = (arr) =>{
+    let snailArray = []
+    while(arr.length){
+        snailArray.push(...arr.shift())
+        for (let i = 0; i < arr.length; i++){
+        snailArray.push(arr[i].pop())
+        }
+        snailArray.push(...(arr.pop() || []).reverse())
+        for (var i = arr.length -1; i >= 0; i--){
+        snailArray.push(arr[i].shift())
+        }
+    }
+    console.log(snailArray) 
+    }
+
+snail([[1,2,3], [4,5,6], [7,8,9]]); 
+// expected output [1,2,3,6,9,8,7,4,5]
